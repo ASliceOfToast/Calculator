5e211d74dfab3545913f11ac488c5aa6e45616d5
wizzardofsoz <whopointseven@gmail.com>
2016-04-23 19:26:29 -0400

    Added files via upload

--- src/main/java/cs1302/calc/IterativeMath.java
+++ src/main/java/cs1302/calc/IterativeMath.java
@@ -1,46 +1,111 @@
-package main.java.cs1302.calc;
+package cs1302.calc;
 
 /* Implement the Math interface using iteration here. For each method, replace
  * the throw statement with your implementation.
  */
 
 /**
- * REPLACE ME WITH ACTUAL JAVADOC FOR THIS CLASS.
+ * Implements various mathematical operations over non-negative integers using iterative methods.
  *
- * @author AUTHOR
+ * @author TRINITY KEY & NATHAN SU
  */
 public class IterativeMath implements Math {
 
-    public int inc(int n) {
+    /*public int inc(int n) {
         throw new UnsupportedOperationException();
     } // inc
 
     public int dec(int n) {
         throw new UnsupportedOperationException();
     } // dec
-
+   */
     public int add(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        while(rhs != 0){
+         lhs = inc(lhs);
+         rhs = dec(rhs);
+        }
+        return lhs;
     } // add
 
     public int sub(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        while(rhs != 0){
+         lhs = dec(lhs);
+         rhs = dec(rhs);
+        }
+        return lhs;
     } // sub
 
     public int mul(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(lhs == 0 || rhs == 0){
+         return 0;
+        }
+        else if(lhs == 1){
+         return rhs;
+        }
+        else if(rhs == 1){
+         return lhs;
+        }
+        else{
+         int bs = 0;
+         for(int i = rhs;i > 0;i = dec(i)){
+           
+           bs = add(bs,lhs);
+           
+         }
+         return bs;
+        }
     } // mul
 
     public int div(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(rhs == 1){
+         return lhs;
+        }
+        else if(lhs == rhs){
+         return 1;
+        }
+        else if(lhs < rhs){
+         return 0;
+        }
+        else{
+         int c = 0;
+         while(lhs >= rhs){
+            lhs = sub(lhs,rhs);
+            c = inc(c);
+         }
+         return c;
+        }
     } // div
 
     public int fac(int n) {
-        throw new UnsupportedOperationException();
+        if(n == 0){
+         return 1;
+        }
+        else if(n == 1){
+         return 1;
+        }
+        else{
+         for(int i = n - 1; i > 0; i = dec(i)){
+          n = mul(n,i);
+         }
+         return n;
+        }
     } // fac
 
     public int pow(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(rhs == 0){
+         return 1;
+        }
+        else if(rhs == 1){
+         return lhs;
+        }
+        else{
+         int bs = lhs;
+         while(rhs > 1){
+            lhs = mul(lhs,bs);
+            rhs = dec(rhs);
+         }
+         return lhs;
+        }
     } // pow
 
 } // IterativeMath
--- src/main/java/cs1302/calc/RecursiveMath.java
+++ src/main/java/cs1302/calc/RecursiveMath.java
@@ -1,46 +1,84 @@
-package main.java.cs1302.calc;
+package cs1302.calc;
 
 /* Implement the Math interface using recursion here. For each method, replace
  * the throw statement with your implementation.
  */
 
 /**
- * REPLACE ME WITH ACTUAL JAVADOC FOR THIS CLASS.
+ * Implements various mathematical operations over non-negative integers using recursive methods.
  *
- * @author AUTHOR
+ * @author TRINITY KEY & NATHAN SU
  */
 public class RecursiveMath implements Math {
 
-    public int inc(int n) {
+    /*public int inc(int n) {
         throw new UnsupportedOperationException();
     } // inc
 
     public int dec(int n) {
         throw new UnsupportedOperationException();
     } // dec
-
+*/
     public int add(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        
+        if(rhs == 0){
+         return lhs; 
+        }
+        return add(inc(lhs),dec(rhs)); 
+        
     } // add
 
     public int sub(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(rhs == 0){
+         return lhs; 
+        }
+        if(rhs > lhs){
+         return 0; 
+        }
+        return sub(dec(lhs),dec(rhs)); 
     } // sub
 
     public int mul(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(lhs == 0 || rhs ==0){
+         return 0; 
+        }
+        if(lhs == 1){
+         return rhs;
+        }
+        if(rhs == 1){
+         return lhs; 
+        }
+        return add(lhs, mul(lhs, dec(rhs))); 
     } // mul
 
     public int div(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(rhs == 1){
+         return lhs;
+        }
+        if(rhs > lhs){
+         return 0;
+        }
+        if(rhs == lhs){
+         return 1;
+        }
+        return 1 + div(sub(lhs,rhs),rhs);
     } // div
 
     public int fac(int n) {
-        throw new UnsupportedOperationException();
+        if(n == 0){
+         return 1;
+        }
+        return mul(n, fac(dec(n)));
     } // fac
 
     public int pow(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(rhs == 0){
+         return 1;
+        }
+        if(rhs == 1){
+         return lhs;
+        }
+        return mul(lhs, pow(lhs,dec(rhs)));
     } // pow
 
 } // RecursiveMath
